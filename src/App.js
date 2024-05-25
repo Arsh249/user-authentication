17f670129090d7ecd2e76bf3fd0ea8033f6da1e5
Arsh Ilahi <arshilahi74@gmail.com>
2024-05-25 16:29:56 +0530

    Files uploaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+// App.js
+import React, { useEffect, useState } from 'react';
+import Login from './components/Login';
+import Profile from './components/Profile';
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState('');
+  const [userId, setUserId] = useState('');
+
+  console.log(token);
+  const handleLogin = (authToken, id) => {
+    setIsLoggedIn(true);
+    setToken(authToken);
+    setUserId(id);
+    // Save token and id to local storage
+    localStorage.setItem('token', authToken);
+    localStorage.setItem('userId', id);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setToken('');
+    setUserId('');
+    // Clear token and id from local storage
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+  };
+
+  // Check if user is already logged in
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    const storedUserId = localStorage.getItem('userId');
+    if (storedToken && storedUserId) {
+      setIsLoggedIn(true);
+      setToken(storedToken);
+      setUserId(storedUserId);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isLoggedIn ? (
+        <Profile id={userId} />
+      ) : (
+        <Login onLogin={handleLogin} />
+      )}
+      {isLoggedIn && <button className='logout' onClick={handleLogout}>Logout</button>}
     </div>
   );
-}
+};
 
 export default App;
+
